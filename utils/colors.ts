8cd76f05c813c53d0557eee7298e5ca2de43142b
covenantcodes <covenantcodes@gmail.com>
2025-03-04 00:12:25 +0100

    added blue color to utility and updated colors.ts

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,11 +1,12 @@
 const colors = {
-    primaryColor: "#22A37C",
-    white: "#ffffff",
-    background: "#FAFAFA",
-    black: "#000000",
-    red: "#DA100B",
-    gray: "#656565",
-    deepBlue:"#0C263A"
+    primaryColor: '#22A37C',
+    white: '#ffffff',
+    background: '#FAFAFA',
+    black: '#000000',
+    red: '#DA100B',
+    gray: '#656565',
+    blue: '#2563EB',
+    deepBlue:'#0C263A'
   };
   
   export default colors;
