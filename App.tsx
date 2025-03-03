6a1412527b3ed6ad9b5b1fdb55620e6f944eeef7
covenantcodes <covenantcodes@gmail.com>
2025-03-03 22:37:09 +0100

    initialize project with Expo, TypeScript, and basic app structure.
    Added Fonts Setup as well

---
+++ App.tsx
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { useFonts } from "expo-font";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    JostThin: require("./assets/fonts/Jost-Thin.ttf"),
+    JostThinItalic: require("./assets/fonts/Jost-ThinItalic.ttf"),
+    JostLight: require("./assets/fonts/Jost-Light.ttf"),
+    JostExtraLight: require("./assets/fonts/Jost-ExtraLight.ttf"),
+    JostMedium: require("./assets/fonts/Jost-Medium.ttf"),
+    JostMediumItalic: require("./assets/fonts/Jost-MediumItalic.ttf"),
+    JostRegular: require("./assets/fonts/Jost-Regular.ttf"),
+    JostSemiBold: require("./assets/fonts/Jost-SemiBold.ttf"),
+    JostBold: require("./assets/fonts/Jost-Bold.ttf"),
+    JostExtraBold: require("./assets/fonts/Jost-ExtraBold.ttf"),
+    JostExtraBoldItalic: require("./assets/fonts/Jost-ExtraBoldItalic.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="small" color="#0000ff" />
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ utils/fonts.ts
@@ -0,0 +1,24 @@
+export const FONTFAMILY = {
+    bold: "JostBold",
+    regular: "JostRegular",
+    light: "JostLight",
+    semibold: "JostSemiBold",
+    heavy: "JostExtraBold",
+    medium: "JostMedium",
+  };
+  
+  export const FONTSIZE = {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 20,
+    xl: 24,
+    xxl: 32,
+  };
+  
+  export const LINEHEIGHT = {
+    tight: 16,
+    normal: 20,
+    relaxed: 24,
+  };
+  
